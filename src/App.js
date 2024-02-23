0e7904dec4fc4c0f42ef99e03236a70ed42f0de6
saivenkat nayini <nayinisaivenkat22@gmail.comgit config --global user.name NayiniSaivenkat>
2024-02-23 19:26:33 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
 
 function App() {
+  const [formData,setFormData]=useState([{
+    FirstName:"",
+    LastName:""
+  }]);
+  const [submitedData,setSubmitedData]=useState(null);
+  const handleChange=(e)=>{
+    const {name,value}=e.target;
+    setFormData((prevVal)=>({
+      ...prevVal,
+      [name]:value
+    }))
+  }
+  const submit=(e)=>{
+    e.preventDefault();
+    if(formData.FirstName && formData.LastName){
+    setSubmitedData(formData)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={submit}>
+      <h1>Full Name Display</h1>
+      <label>
+        First Name:
+        <input 
+        type="text"
+        name='FirstName'
+        value={formData.FirstName}
+        onChange={handleChange}
+        />
+      </label>
+      <br/>
+      <label>
+        Last Name:
+        <input 
+        type="text"
+        name='LastName'
+        value={formData.LastName}
+        onChange={handleChange}
+        />
+      </label>
+      <br/>
+      <button type='submit'>Submit</button>
+      {submitedData && 
+      (
+        <div>
+          <p>Full Name: {submitedData.FirstName} {submitedData.LastName}</p>
+        </div>
+      )
+      }
+    </form>
+    
   );
 }
 
