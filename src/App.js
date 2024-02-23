f96b203ed332b5201474c71d754674f6da948c07
saivenkat nayini <nayinisaivenkat22@gmail.comgit config --global user.name NayiniSaivenkat>
2024-02-23 19:49:28 +0530

    third

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import './App.css';
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const [formData,setFormData]=useState({
-    FirstName:"",
-    LastName:""
+  const [formData, setFormData] = useState({
+    FirstName: "",
+    LastName: ""
   });
-  const [submitedData,setSubmitedData]=useState(null);
-  const handleChange=(e)=>{
-    const {name,value}=e.target;
-    setFormData((prevVal)=>({
+  const [submitedData, setSubmitedData] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevVal) => ({
       ...prevVal,
-      [name]:value
-    }))
+      [name]: value
+    }));
   }
-  const submit=(e)=>{
+
+  const submit = (e) => {
     e.preventDefault();
-    if(formData.FirstName && formData.LastName){
-    setSubmitedData(formData)
+    if (formData.FirstName && formData.LastName) {
+      const submittedFormData = {
+        FirstName: formData.FirstName,
+        LastName: formData.LastName
+      };
+      setSubmitedData(submittedFormData);
     }
   }
 
@@ -26,34 +32,33 @@ function App() {
       <h1>Full Name Display</h1>
       <label>
         First Name:
-        <input 
-        type="text"
-        name='FirstName'
-        value={formData.FirstName}
-        onChange={handleChange}
+        <input
+          type="text"
+          name='FirstName'
+          value={formData.FirstName}
+          onChange={handleChange}
         />
       </label>
-      <br/>
+      <br />
       <label>
         Last Name:
-        <input 
-        type="text"
-        name='LastName'
-        value={formData.LastName}
-        onChange={handleChange}
+        <input
+          type="text"
+          name='LastName'
+          value={formData.LastName}
+          onChange={handleChange}
         />
       </label>
-      <br/>
+      <br />
       <button type='submit'>Submit</button>
-      {submitedData && 
-      (
-        <div>
-          <p>Full Name: {submitedData.FirstName} {submitedData.LastName}</p>
-        </div>
-      )
+      {submitedData &&
+        (
+          <div>
+            <p>Full Name: {submitedData.FirstName} {submitedData.LastName}</p>
+          </div>
+        )
       }
     </form>
-    
   );
 }
 
